49b9d7ae4aec3e7155e162af4a7349aa3fab9a98
vitória <112172698+VitoriaBarroso@users.noreply.github.com>
2025-05-08 15:13:46 -0300

    loops com for e while
    aula de loops for while

---
+++ loops_for_while/while.js
@@ -0,0 +1,8 @@
+const app = document.getElementById("app") // tag inteira pelo id
+const nomes = ["Sabrina", "Vanessa", "Taylor", "Ariana"]
+const idades = [23, 33, 67, 12]
+let numero = 100
+while (true) {
+    numero++
+    console.log(numero)
+}
